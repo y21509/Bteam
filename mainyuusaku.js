558c97c7028dedc1de056e9f92201db3c205dc72
YuusakuPine <96404850+YuusakuPine@users.noreply.github.com>
2022-01-17 14:15:23 +0900

    a
    棒倒し入れました

--- mainyuusaku.js
+++ mainyuusaku.js
@@ -1,7 +1,9 @@
 //canvasの設定
 var canvas = document.getElementById( 'canvas' );
-canvas.width = 640;	//canvasの横幅（よこはば）
-canvas.height = 640;	//canvasの縦幅（たてはば）
+var w = 55;
+var h = 35;
+canvas.width = w*16;	//canvasの横幅（よこはば）
+canvas.height = h*16;	//canvasの縦幅（たてはば）
 
 //コンテキストの取得
 var ctx = canvas.getContext('2d');
@@ -14,12 +16,19 @@ mazeroad.src = 'img/mazeroad.jpg';
 var mazewall = new Image();
 mazewall.src = 'img/mazewall.png';
 
+//ゴールのImageオブジェクト
+var goal = new Object();
+goal.img = new Image();
+goal.img.src = 'img/goal.jpg';
+goal.x=50*16;
+goal.y=32*16;
+
 //プレイヤーのオブジェクト作成
 var player = new Object();
 player.img = new Image();
 player.img.src = 'img/player.jpg'
-player.x=0;
-player.y=0;
+player.x=2*16;
+player.y=2*16;
 player.move=0;
 
 //キーボードのオブジェクト作成
@@ -30,85 +39,115 @@ key.right = false;
 key.left = false;
 key.push = '';
 
-//迷路の作成
-var res = [
-    [0, 0, 1, 0, 1, 0, 0, 0 ,0 ,1 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ,1 ,0],
-	[0, 1, 0, 0, 0, 1, 1, 1 ,0 ,1 ,0 ,1 ,1 ,0 ,1 ,1 ,1 ,0 ,1 ,0],
-	[0, 0, 1, 1, 0, 0, 0, 1 ,0 ,0 ,0 ,1 ,0 ,0 ,0 ,1 ,0 ,0 ,0 ,0],
-	[1, 0, 1, 0, 1, 1, 0, 0 ,0 ,1 ,1 ,1 ,1 ,1 ,0 ,0 ,1 ,0 ,1 ,0],
-	[0, 0, 0, 0, 0, 1, 1, 1 ,0 ,1 ,0 ,0 ,0 ,0 ,1 ,1 ,0 ,1 ,1 ,0],
-	[0, 1, 1, 1, 0, 0, 0, 0 ,0 ,1 ,0 ,1 ,1 ,1 ,0 ,1 ,0 ,0 ,0 ,0],
-	[0, 1, 1, 1, 0, 1, 1, 1 ,1 ,1 ,0 ,1 ,0 ,0 ,0 ,0 ,1 ,1 ,1 ,0],
-	[0, 0, 0, 1, 0, 0, 0, 0 ,1 ,0 ,0 ,1 ,0 ,1 ,1 ,0 ,0 ,0 ,1 ,0],
-	[1, 1, 0, 1, 1, 1, 1, 1 ,1 ,0 ,1 ,1 ,0 ,0 ,1 ,1 ,1 ,0 ,1 ,1],
-	[1, 0, 0, 0, 0, 0, 1, 1 ,0 ,0 ,0 ,0 ,1 ,0 ,1 ,1 ,0 ,0 ,1 ,0],
-	[1, 0, 1, 1, 1, 0, 0, 0 ,1 ,0 ,1 ,0 ,0 ,0 ,0 ,0 ,1 ,1 ,0 ,0],
-	[1, 0, 1, 0, 1, 1, 1, 0 ,1 ,0 ,1 ,1 ,0 ,1 ,1 ,0 ,0 ,0 ,0 ,1],
-	[0, 0, 1, 0, 0, 1, 0, 0 ,1 ,0 ,0 ,1 ,0 ,1 ,0 ,1 ,1 ,1 ,0 ,0],
-	[0, 1, 1, 1, 0, 1, 0, 1 ,0 ,0 ,1 ,1 ,0 ,1 ,0 ,1 ,1 ,0 ,1 ,0],
-	[0, 0, 0, 1, 0, 1, 0, 0 ,1 ,0 ,1 ,1 ,0 ,1 ,0 ,0 ,0 ,0 ,0 ,0],
-	[1, 1, 0, 1, 0, 1, 0, 1 ,1 ,0 ,0 ,1 ,0 ,1 ,1 ,0 ,1 ,1 ,1 ,0],
-	[0, 0, 0, 1, 0, 1, 1, 1 ,1 ,1 ,0 ,1 ,0 ,1 ,1 ,0 ,0 ,0 ,1 ,0],
-	[0, 1, 1, 1, 0, 1, 0, 0 ,0 ,0 ,0 ,1 ,0 ,0 ,0 ,1 ,1 ,0 ,1 ,1],
-	[0, 1, 0, 0, 0, 1, 0, 1 ,1 ,1 ,0 ,0 ,1 ,1 ,0 ,1 ,0 ,0 ,0 ,0],
-	[0, 0, 0, 1, 0, 0, 0, 1 ,1 ,1 ,1 ,0 ,0 ,0 ,1 ,1 ,1 ,1 ,1 ,0]
-]
+var maze = new Array(w * h);
+    for (var y = 1; y < h - 1; y ++) {
+        for (var x = 1; x < w - 1; x ++) {
+            maze[x + w * y] = 1;
+        }
+    }
+    
+// 開始位置と方向とパターン
+var startX = w - 5;        // 固定位置
+var startY = 4;            // 固定位置
+var dir = [[-1, 0], [0, -1], [1, 0], [0, 1]];
+var pattern = 
+    [[0, 1, 2, 3]
+    ,[0, 1, 3, 2]
+    ,[0, 2, 1, 3]
+    ,[0, 2, 3, 1]
+    ,[0, 3, 1, 2]
+    ,[0, 3, 2, 1]
+    ,[1, 0, 2, 3]
+    ,[1, 0, 3, 2]
+    ,[1, 2, 0, 3]
+    ,[1, 2, 3, 0]
+    ,[1, 3, 0, 2]
+    ,[1, 3, 2, 0]
+    ,[2, 0, 1, 3]
+    ,[2, 0, 3, 1]
+    ,[2, 1, 0, 3]
+    ,[2, 1, 3, 0]
+    ,[2, 3, 0, 1]
+    ,[2, 3, 1, 0]
+    ,[3, 0, 1, 2]
+    ,[3, 0, 2, 1]
+    ,[3, 1, 0, 2]
+    ,[3, 1, 2, 0]
+    ,[3, 2, 0, 1]
+    ,[3, 2, 1, 0]];
+
+// 穴掘り法
+function dig(x, y) {
+    // ランダムを使わずに生成
+    var type = (x + 3) * (y + 5) * 7 % pattern.length;
+    for (var i = 0; i < dir.length; i++) {
+        var next = dir[pattern[type][i]];
+        if (maze[(x + next[0] * 2) + w * (y + next[1] * 2)] == 1) {
+            maze[(x + next[0] * 2) + w * (y + next[1] * 2)] = 0;
+            maze[(x + next[0]    ) + w * (y + next[1]    )] = 0;
+            dig(x + next[0] * 2, y + next[1] * 2);
+        }
+    }
+}
+dig(startX, startY);
 
 function main(){
     ctx.fillStyle = "rgb(0,0,0)";
 
-    ctx.fillRect(0,0,640,640);
+    ctx.fillRect(0,0,880,560);
 
-    for(var y=0; y<res.length; y++){
-        for(var x=0; x<res[y].length; x++){
-            if(res[y][x]===0)ctx.drawImage(mazeroad,0,0,32,32,32*x,32*y,32,32);
-            if(res[y][x]===1)ctx.drawImage(mazewall,0,0,32,32,32*x,32*y,32,32);
+    for(var y=0; y<h; y++){
+        for(var x=0; x<w; x++){
+            if(maze[x+w*y]===0)ctx.drawImage(mazeroad,0,0,16,16,16*x,16*y,16,16);
+            if(maze[x+w*y]===1)ctx.drawImage(mazewall,0,0,16,16,16*x,16*y,16,16);
         }
     }
 
-    //画像表示
+    //プレイヤー表示
     ctx.drawImage( player.img,player.x,player.y);
+    //ゴール表示
+    ctx.drawImage( goal.img,goal.x,goal.y);
 
     addEventListener("keydown",keydownfunc,false);
     addEventListener("keyup",keyupfunc,false);
 
     if(player.move === 0){            
         if ( key.left === true ) {
-            var x = player.x/32;
-            var y = player.y/32;
+            var x = player.x/16;
+            var y = player.y/16;
             x--;
-            if( res[y][x] === 0 ){
-                player.move = 32;
+            if( maze[x+w*y] === 0 ){
+                player.move = 16;
                 key.push = 'left';
             }
 		}
 		if ( key.up === true ) {
-			var x = player.x/32;
-            var y = player.y/32;
+			var x = player.x/16;
+            var y = player.y/16;
             if( y !== 0 ){
                 y--;
-            if( res[y][x] === 0 ){
-                player.move = 32;
+            if( maze[x+w*y] === 0 ){
+                player.move = 16;
                 key.push = 'up';
             }
             }
 		}
 		if ( key.right === true ) {
-			var x = player.x/32;
-            var y = player.y/32;
+			var x = player.x/16;
+            var y = player.y/16;
             x++;
-            if( res[y][x] === 0 ){ 
-                player.move = 32;
+            if( maze[x+w*y] === 0 ){ 
+                player.move = 16;
                 key.push = 'right';
             }
 		}
 		if ( key.down === true ) {
-			var x = player.x/32;
-            var y = player.y/32;
-            if( y !== 19){
+			var x = player.x/16;
+            var y = player.y/16;
+            if( y !== 34){
                 y++;
-            if( res[y][x] === 0 ){
-                player.move = 32;
+            if( maze[x+w*y] === 0 ){
+                player.move = 16;
                 key.push = 'down';
             }
             }
@@ -122,10 +161,13 @@ function main(){
         if(key.push === 'right')player.x += 4;
         if(key.push === 'down')player.y += 4;
     }
+    //ゴール処理
+    if(player.x===goal.x||player.y===goal.y){
+
+    }
 
     requestAnimationFrame( main );
 }
-
 
 addEventListener('load',main(),false);
 
